fa913d2c6293cbe99ae565a2401c3811ef1808aa
akutsang <akuutsang@gmail.com>
2025-01-22 21:43:30 +0100

    make Agric section responsive

--- src/components/chooseUs/page.js
+++ src/components/chooseUs/page.js
@@ -4,43 +4,40 @@ import { Item } from "../data";
 function Choose() {
   return (
     <>
-      <div className="w-full bg-[#ECFDF5] ">
+      <div className="w-full bg-[#ECFDF5] pb-6">
         <p className="font-[550] text-xl text-[#000000] flex justify-center w-full mb-[30px] h-[43px] gap-9 items-center ">
           Why choose us
         </p>
-        <div className=" w-full flex   justify-center">
-          <div className="w-[750px]  flex justify-center gap-4 ">
-            <div className="w-[610px] h-[562px]">
+        <div className=" w-full flex justify-center">
+          <div className="md:w-[750px]  flex md:flex-row justify-center md:gap-4 w-full px-6  flex-col">
+            <div className="md:w-[610px] md:h-[562px]">
               <img
                 src="/images/Frame39.png"
                 alt="farmer"
-                className="w-full h-[450px] object-cover rounded-[10px]"
+                className="w-full h-[450px] object-cover rounded-[35px] pb-4"
               />
             </div>
-            <div className=" w-[700px] h-[450px] bg-[#40984440] rounded-[10px] flex flex-col justify-center items-center">
-              <div className="w-[90%] h-[395px] my-4">
+            <div className=" md:w-[700px] md:h-[450px] bg-[#40984440] w-full rounded-[10px] flex flex-col justify-center items-center">
+              <div className="w-[90%] md:h-[395px] md:my-4 ">
                 {Item.map((items) => (
-                  <>
-                    <div
-                      key={items.name}
-                      className=" flex justify-between items-center font-playfair font-medium"
-                    >
-                      <p className=" text-[#000000] my-4 text-[15.5px]">
+                  <div key={items.name}>
+                    <div className="flex justify-between items-center font-playfair font-medium">
+                      <p className="text-[#000000] my-4 text-[15.5px]">
                         {items.text}
                       </p>
                       <img
                         src={items.photo}
-                        alt="items.title"
+                        alt={items.title}
                         className="w-[46.83px] h-[40px]"
                       />
                     </div>
-                    <div className="">
-                      <hr />
+                    <div>
+                      <hr className="border-black border-1" />
                     </div>
-                  </>
+                  </div>
                 ))}
               </div>
-              <div className="w-[250px] h-[50px] flex items-center ">
+              <div className="w-[250px] h-[50px] flex items-center my-3 ">
                 <button className="h-[10px] w-[250px] py-[20px] px-[9px] font-Sans flex justify-center items-center text-[black] bg-[#F9DF02] rounded-[20px] text-xs">
                   Learn More About Our Story
                 </button>
--- src/components/services/page.js
+++ src/components/services/page.js
@@ -5,7 +5,7 @@ function Services() {
   return (
     <div>
       <>
-        <div className="md:h-full md:w-full flex flex-col justify-center bg-[#ECFDF5]">
+        <div className="md:h-full md:w-full flex flex-col justify-center bg-[#ECFDF5] px-6">
           <div className="md:w-[1232px] md:h-[117px]  py-6 w-full text-black flex flex-col justify-center items-center">
             <p className="font-playfair font-[150px] text-[24px] ">
               Our Services
@@ -16,25 +16,29 @@ function Services() {
             </p>
           </div>
           <div className="flex justify-center items-center  md:w-[1232px] h-full w-full">
-            <div className="w-[1100px] grid grid-cols-2 md:grid-cols-3   items-center">
+            <div className="w-[1100px] grid grid-cols-2 md:grid-cols-3 justify-center  items-center rounded-[20px]">
               {Card.map((cards) => (
                 <div
                   key={cards.title}
-                  className=" w-[300px] font-playfair h-[405px]  flex flex-col md:bg-white text-[#000000] rounded-[20px] justify-between md:p-5 p-4"
+                  className=" w-[300px] font-playfair  flex flex-col md:bg-white text-[#000000] justify-between md:p-5 p-2 "
                 >
-                  <p className="text-[24px] h-[43px] font-playfair font-[65px] text-center">
-                    {cards.title}
-                  </p>
-                  <div className="h-[250px] w-full">
-                    <img
-                      src={cards.photo}
-                      alt={cards.title}
-                      className="w-full h-full object-cover rounded-[10px]"
-                    />
+                  <div className="rounded-[10px] bg-white">
+                    <p className="text-[24px] h-[43px] font-playfair font-[65px] text-center ">
+                      {cards.title}
+                    </p>
+                    <div className="h-[400px] w-full b">
+                      <img
+                        src={cards.photo}
+                        alt={cards.title}
+                        className="w-full h-[250px] object-cover pb-3"
+                      />
+                      <div className="md:w-full  flex justify-center ">
+                        <p className="md:font-[400] h-[150px]  w-[80%]  font-sans text-center text-s leading-[20.11px]">
+                          {cards.text}
+                        </p>
+                      </div>
+                    </div>
                   </div>
-                  <p className="font-[400] h-[80px] font-sans text-center text-xs leading-[20.11px]">
-                    {cards.text}
-                  </p>
                 </div>
               ))}
             </div>
